dace20dfdf3072cb47f7bfd4726b3cdbeee692ad
Faisal Kanout <faisal@kanout.com>
2020-12-05 14:01:57 +0100

    ADD - CORS and helmet

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
 import Koa from 'koa';
+import cors from '@koa/cors';
+import helmet from 'koa-helmet';
 import { genericRouter, router } from './routers';
 
 const app = new Koa();
 
+app.use(helmet());
+app.use(cors());
 app.use(router.routes());
 app.use(genericRouter.routes());
 app.use(router.allowedMethods());
